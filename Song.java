092887d7487a95f033cf31b9e5b4a8d6c99322fd
Bryan Vuong <vuong.bryan@gmail.com>
2018-01-17 22:41:57 -0600

    Adding All Pertinent Files and Code

---
+++ Song.java
@@ -0,0 +1,166 @@
+
+/**
+*  author: Bryan Vuong
+*  date:  09/14/2010
+*  CS 305j Assignment 2
+*  On my honor, Bryan Vuong, this programming assignment is my own work.
+*
+*  EID: BKV85
+*  Section: 52263, T 3-4
+*
+* The program prints the lyrics to the song, "The Twelve Days of Christmas"
+*
+* Slip Days I am using on this project: 0
+* Slip Days I have used this semester: 0
+*/
+
+public class Song {
+    public static void main(String[] args)
+    {
+    first();
+    partridge();
+    second();
+    doves();
+    third();
+    hens();
+    fourth();
+    birds();
+    fifth();
+    rings();
+    sixth();
+    geese();
+    seventh();
+    swans();
+    eighth();
+    maids();
+    ninth();
+    ladies();
+    tenth();
+    lords();
+    eleventh();
+    pipers();
+    twelfth();
+    drummers();
+    }
+    //First Day
+    public static void first()
+    {
+        System.out.println ("On the first day of Christmas,\nMy true love sent to me");
+    }
+    public static void partridge()
+    {
+        System.out.println("A partridge in a pear tree.\n");
+    }
+    //Second Day
+    public static void second()
+    {
+        System.out.println("On the second day of Christmas,\nMy true love sent to me");
+    }
+    public static void doves()
+    {
+        System.out.println("Two turtle-doves and");
+        partridge();
+    }
+    //Third Day
+    public static void third()
+    {
+        System.out.println("On the third day of Christmas,\nMy true love sent to me");
+    }
+    public static void hens()
+    {
+        System.out.println("Three French hens,");
+        doves();
+    }
+    //Fourth Day
+    public static void fourth()
+    {
+        System.out.println("On the fourth day of Christmas,\nMy true love sent to me");
+    }
+    public static void birds()
+    {
+        System.out.println("Four calling birds,");
+        hens();
+    }
+    //Fifth Day
+    public static void fifth()
+    {
+        System.out.println("On the fifth day of Christmas,\nMy true love sent to me");
+    }
+    public static void rings()
+    {
+        System.out.println("Five golden rings,");
+        birds();
+    }
+    //Sixth Day
+    public static void sixth()
+    {
+        System.out.println("On the sixth day of Christmas,\nMy true love sent to me");
+    }
+    public static void geese()
+    {
+        System.out.println("Six geese a-laying,");
+        rings();
+    }
+    //Seventh Day
+    public static void seventh()
+    {
+        System.out.println("On the seventh day of Christmas,\nMy true love sent to me");
+    }
+    public static void swans()
+    {
+        System.out.println("Seven swans a-swimming,");
+        geese();
+    }
+    //Eighth Day
+     public static void eighth()
+    {
+        System.out.println("On the eighth day of Christmas,\nMy true love sent to me");
+    }
+    public static void maids()
+    {
+        System.out.println("Eight maids a-milking,");
+        swans();
+    }
+    //Ninth Day
+    public static void ninth()
+    {
+        System.out.println("On the ninth day of Christmas,\nMy true love sent to me");
+    }
+    public static void ladies()
+    {
+        System.out.println("Nine ladies dancing,");
+        maids();
+    }
+    //Tenth Day
+    public static void tenth()
+    {
+        System.out.println("On the tenth day of Christmas,\nMy true love sent to me");
+    }
+    public static void lords()
+    {
+        System.out.println("Ten lords a-leaping,");
+        ladies();
+    }
+    //Eleventh Day
+    public static void eleventh()
+    {
+        System.out.println("On the eleventh day of Christmas,\nMy true love sent to me");
+    }
+    public static void pipers()
+    {
+        System.out.println("Eleven pipers piping,");
+        lords();
+    }
+    //Twelfth Day
+    public static void twelfth()
+    {
+        System.out.println("On the twelfth day of Christmas,\nMy true love sent to me");
+    }
+    public static void drummers()
+    {
+        System.out.println("Twelve drummers drummings,");
+        pipers();
+    }
+}
+
+    
